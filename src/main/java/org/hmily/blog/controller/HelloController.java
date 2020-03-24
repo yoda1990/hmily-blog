939798bb576096982f2b3583e9bb9b3fea3b6412
You-Hmily <706097141@qq.com>
2020-03-24 21:02:15 +0800

    页面原型完成

---
+++ src/main/java/org/hmily/blog/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.hmily.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello World 控制器
+ */
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+	    return "Hello World! Welcome to visit waylau.com!";
+	}
+}
